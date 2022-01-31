f4473b53ffd918ab2979939f18ac8f1d65cd82b8
Abhishek <abhisheksinghcsanand@gmail.com>
2022-02-01 00:24:41 +0530

    learned nextjs no css used

---
+++ abhi-app/pages/Header.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+const Header = () => {
+  return (
+    <Head>
+      <title>my page</title>
+      <link rel="shortcut icon" href="/My Pic.jpg"></link>
+    </Head>
+  );
+}
+
+export default Header;
--- abhi-app/pages/index.js
+++ abhi-app/pages/index.js
@@ -1,7 +1,7 @@
 import Navbar from '../pages/components/Navbar';
-import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
+import Header from './Header';
 export const getStaticProps = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
   const data = await res.json();
@@ -14,9 +14,7 @@ export const getStaticProps = async () => {
 const index = ({ data }) => {
   return (
     <div>
-      <Head>
-        <title>my page</title>
-      </Head>
+      <Header />
       <h1> Hello Abhishek</h1>
       <Navbar />
       <Image src="https://images.unsplash.com/photo-1612646785818-defa3c0c19f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" width="500" height="500"></Image>
